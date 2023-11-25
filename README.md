eab7bcc7c8859146e7409444e3a041fa64c66aa6
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2023-11-26 05:50:15 +0800

    working and perfectly green

---
+++ README.md
@@ -0,0 +1,5 @@
+# collage
+
+## Prepare Photos For The Internet
+
+### Renames pictures in a numerical ascending order and swipes all metadata from them
--- perform.rb
+++ perform.rb
@@ -1,8 +1,25 @@
 #!/usr/bin/env ruby 
 
+
+require 'colorize'
+require 'pry'
+
+def print_info(str)
+  raise 'empty info' if str == nil
+
+  puts str.colorize(color: :green, mode: :bold)
+end
+
 def perform
   counter = 0
-  all_images.each_with_index { |file_name, idx|  rename(file_name, idx + 1); counter += 1 }
+
+  all_images.each_with_index { 
+    |file_name, idx|  
+      remove_metadata(file_name);
+      rename(file_name, idx + 1); 
+      counter += 1 
+  }
+
   counter
 end 
 
@@ -10,15 +27,22 @@ def rename(file_name, n)
  File.rename(file_name, n.to_s + File.extname(file_name))
 end
 
-def. remove_metadata
-  system exiftool -all= -overwrite_original_in_place "${FILENAME}"
+def remove_metadata(file_name)
+  system "exiftool -all= -overwrite_original_in_place #{file_name} > /dev/null"
 end 
 
 def all_images
   Dir.glob('*.{jpeg,jpg,png}')
 end 
 
+def usage
+  print_info "\nUsage: #{$0}\n\nResult:\n[+] renamed 5 pictures\n\n" 
+  exit
+end 
 
+usage unless ARGV.size != 1
 
-puts "\n[+] renamed #{perform} pictures\n"
+  counter = perform
+
+  print_info("\n[+] processed #{counter} pictures\n")
 
