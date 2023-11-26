1ebb3bfe8fd1ba30d2faf24d89723805fe7db54d
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2023-11-26 09:34:16 +0800

    help and usage__ commands display usage info

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
+gem 'pry', '~> 0.14.2'
 gem 'rspec', '~> 3.5'
-gem 'rubocop', '~> 1.43'
+gem 'rubocop', '~> 1.57', '>= 1.57.2'
 gem 'colorize', '~> 1.1'
-gem 'pry'
+
--- app.rb
+++ app.rb
@@ -2,10 +2,36 @@
 
 require_relative './collage_proccessor.rb'
 
-def app_args
-  [ARGV.first || '.']
+def first_arg
+  @first_arg ||= ARGV.first
+end 
+
+def need_help?
+  if help_is_not_directory_or_existing_file?
+    return true if help_args_passed?
+  end 
+  
+  return false
 end
 
-::CollageProccessor.usage if app_args.size != 1
+def help_args_passed?
+  first_arg == 'help' || first_arg == 'usage__'
+end 
 
-CollageProccessor.new(app_args.first)
+def help_is_not_directory_or_existing_file?
+  return help_folder_detected_message if help_node_exist?
+  true
+end 
+
+def help_node_exist?
+  Dir['help'].size.positive? 
+end 
+
+def help_folder_detected_message 
+  puts "\n\n'help' filenode detected. Reference can be requested in this way:\n #{$0} usage__\n\n".colorize(:light_blue)
+  false
+end
+
+::CollageProccessor.usage && exit if need_help?
+
+CollageProccessor.new(first_arg)
